4d4eff9609f1bc59eb8656e3c4d82de5aa72c1f1
Henrik Grimler <henrik@grimler.se>
2021-11-26 22:42:33 +0100

    termux-usb: add option to pass file descriptor as env var
    And remove termux-callback shell script, we can implement that
    function in termux-api.c.

--- Makefile
+++ Makefile
@@ -6,10 +6,9 @@ termux-api: termux-api.c
 install: termux-api
 	mkdir -p $(PREFIX)/bin/ $(PREFIX)/libexec/
 	install termux-api $(PREFIX)/libexec/
-	install termux-callback $(PREFIX)/libexec/
 	cd scripts; for i in *; do \
-	  sed -e "s|@TERMUX_PREFIX@|$(PREFIX)|g" $$i > $(PREFIX)/bin/$$i; \
-	  chmod 700 $(PREFIX)/bin/$$i; \
+		sed -e "s|@TERMUX_PREFIX@|$(PREFIX)|g" $$i > $(PREFIX)/bin/$$i; \
+		chmod 700 $(PREFIX)/bin/$$i; \
 	done
 
 .PHONY: install
--- termux-api.c
+++ termux-api.c
@@ -69,9 +69,27 @@ _Noreturn void exec_am_broadcast(int argc, char** argv, char* input_address_stri
 _Noreturn void exec_callback(int fd)
 {
     char *fds;
-    if(asprintf(&fds, "%d", fd) == -1) { perror("asprintf"); }
-    execl(PREFIX "/libexec/termux-callback", "termux-callback", fds, NULL);
-    perror("execl(\"" PREFIX "/libexec/termux-callback\")");
+    char *callback_fun = getenv("TERMUX_CALLBACK");
+    if (callback_fun == NULL) {
+        perror("TERMUX_CALLBACK is not set");
+        exit(1);
+    }
+
+    if (asprintf(&fds, "%d", fd) == -1)
+        perror("asprintf");
+
+    char errmsg[256];
+    char *export_to_env = getenv("TERMUX_EXPORT_FD");
+    if (strncmp(export_to_env, "true", 4) == 0) {
+        if (setenv("TERMUX_USB_FD", fds, true) == -1)
+            perror("setenv");
+        execl(callback_fun, callback_fun, NULL);
+        sprintf(errmsg, "execl(\"%s\", %s)", callback_fun, fds);
+    } else {
+        execl(callback_fun, callback_fun, fds, NULL);
+        sprintf(errmsg, "execl(\"%s\")", callback_fun);
+    }
+    perror(errmsg);
     exit(1);
 }
 
