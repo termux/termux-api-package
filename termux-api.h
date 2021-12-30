d6972ebecd1e716cd77d56a1493f04522783dfc0
Henrik Grimler <grimler@termux.dev>
2021-12-30 09:54:03 +0100

    termux-api.h: allow usage from c++
    And prevent header from being included multiple times for no reason.

--- termux-api.h
+++ termux-api.h
@@ -1,4 +1,11 @@
+#ifndef TERMUX_API_H
+#define TERMUX_API_H
+
 #include <stdlib.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
 _Noreturn void exec_am_broadcast(int, char**, char*, char*);
 _Noreturn void contact_plugin(int, char**, char*, char*);
@@ -7,3 +14,9 @@ void generate_uuid(char*);
 void* transmit_stdin_to_socket(void*);
 int transmit_socket_to_stdout(int);
 int run_api_command(int, char**);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* TERMUX_API_H */
