termux/termux-api-package

commit 1ec1334b54e0bb6a2c60048e8ebb5b4cd0cf8543

Initial push

termux-api.c
@@ -1,0 +1,156 @@
+// termux-api.c - helper binary for calling termux api classes
+// Usage: termux-api ${API_METHOD} ${ADDITIONAL_FLAGS}
+//        This executes
+//          am broadcast com.termux.api/.TermuxApiReceiver --es socket_input ${INPUT_SOCKET} 
+//                                                        --es socket_output ${OUTPUT_SOCKET}
+//                                                        --es ${API_METHOD}
+//                                                        ${ADDITIONAL_FLAGS}
+//        where ${INPUT_SOCKET} and ${OUTPUT_SOCKET} are addresses to linux abstract namespace sockets,
+//        used to pass on stdin to the java implementation and pass back output from java to stdout.
+#define _POSIX_SOURCE
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <time.h>
+#include <unistd.h>
+
+// Function which execs "am broadcast ..".
+_Noreturn void exec_am_broadcast(int argc, char** argv, char* input_address_string, char* output_address_string)
+{
+    // Redirect stdout to /dev/null (but leave stderr open):
+    close(STDOUT_FILENO);
+    open("/dev/null", O_RDONLY);
+    // Close stdin:
+    close(STDIN_FILENO);
+
+    int const extra_args = 15; // Including ending NULL.
+    char** child_argv = malloc((sizeof(char*)) * (argc + extra_args));
+
+    child_argv[0] = "am";
+    child_argv[1] = "broadcast";
+    child_argv[2] = "--user";
+    child_argv[3] = "0";
+    child_argv[4] = "-n";
+    child_argv[5] = "com.termux.api/.TermuxApiReceiver";
+    child_argv[6] = "--es";
+    // Input/output are reversed for the java process (our output is its input):
+    child_argv[7] = "socket_input";
+    child_argv[8] = output_address_string;
+    child_argv[9] = "--es";
+    child_argv[10] = "socket_output";
+    child_argv[11] = input_address_string;
+    child_argv[12] = "--es";
+    child_argv[13] = "api_method";
+    child_argv[14] = argv[1];
+
+    // Copy the remaining arguments -2 for first binary and second api name:
+    memcpy(child_argv + extra_args, argv + 2, (argc-1) * sizeof(char*));
+
+    // End with NULL:
+    child_argv[argc + extra_args] = NULL;
+
+    // Use an a executable taking care of PATH and LD_LIBRARY_PATH:
+    execv("/data/data/com.termux/files/usr/bin/am", child_argv);
+
+    perror("execv(\"/data/data/com.termux/files/usr/bin/am\")");
+    exit(1);
+}
+
+void generate_uuid(char* str) {
+    sprintf(str, "%x%x-%x-%x-%x-%x%x%x",
+            arc4random(), arc4random(),                 // Generates a 64-bit Hex number
+            (uint32_t) getpid(),                        // Generates a 32-bit Hex number
+            ((arc4random() & 0x0fff) | 0x4000),         // Generates a 32-bit Hex number of the form 4xxx (4 indicates the UUID version)
+            arc4random() % 0x3fff + 0x8000,             // Generates a 32-bit Hex number in the range [0x8000, 0xbfff]
+            arc4random(), arc4random(), arc4random());  // Generates a 96-bit Hex number
+}
+
+// Thread function which reads from stdin and writes to socket.
+void* transmit_stdin_to_socket(void* arg) {
+    int output_server_socket = *((int*) arg);
+    struct sockaddr_un remote_addr;
+    socklen_t addrlen = sizeof(remote_addr);
+    int output_client_socket = accept(output_server_socket, (struct sockaddr*) &remote_addr, &addrlen);
+
+    ssize_t len;
+    char buffer[1024];
+    while (len = read(STDIN_FILENO, &buffer, sizeof(buffer)), len > 0) {
+        if (write(output_client_socket, buffer, len) < 0) break;
+    }
+    // Close output socket on end of input:
+    close(output_client_socket);
+    return NULL;
+}
+
+// Main thread function which reads from input socket and writes to stdout.
+void transmit_socket_to_stdout(int input_socket_fd) {
+    ssize_t len;
+    char buffer[1024];
+    while ((len = read(input_socket_fd, &buffer, sizeof(buffer))) > 0) {
+        write(STDOUT_FILENO, buffer, len);
+    }
+    if (len < 0) perror("read()");
+}
+
+int main(int argc, char** argv) {
+    // Do not transform children into zombies when they terminate:
+    struct sigaction sigchld_action = { .sa_handler = SIG_DFL, .sa_flags = SA_RESTART | SA_NOCLDSTOP | SA_NOCLDWAIT };
+    sigaction(SIGCHLD, &sigchld_action, NULL);
+
+    char input_address_string[100];  // This program reads from it.
+    char output_address_string[100]; // This program writes to it.
+
+    generate_uuid(input_address_string);
+    generate_uuid(output_address_string);
+
+    struct sockaddr_un input_address = { .sun_family = AF_UNIX };
+    struct sockaddr_un output_address = { .sun_family = AF_UNIX };
+    // Leave struct sockaddr_un.sun_path[0] as 0 and use the UUID string as abstract linux namespace:
+    strncpy(&input_address.sun_path[1], input_address_string, strlen(input_address_string));
+    strncpy(&output_address.sun_path[1], output_address_string, strlen(output_address_string));
+
+    int input_server_socket = socket(AF_UNIX, SOCK_STREAM|SOCK_CLOEXEC, 0);
+    if (input_server_socket == -1) { perror("socket()"); return 1; }
+    int output_server_socket = socket(AF_UNIX, SOCK_STREAM|SOCK_CLOEXEC, 0);
+    if (output_server_socket == -1) { perror("socket()"); return 1; }
+
+    if (bind(input_server_socket, (struct sockaddr*) &input_address, sizeof(sa_family_t) + strlen(input_address_string) + 1) == -1) {
+        perror("bind(input)");
+        return 1;
+    }
+    if (bind(output_server_socket, (struct sockaddr*) &output_address, sizeof(sa_family_t) + strlen(output_address_string) + 1) == -1) {
+        perror("bind(output)");
+        return 1;
+    }
+
+    if (listen(input_server_socket, 1) == -1) { perror("listen()"); return 1; }
+    if (listen(output_server_socket, 1) == -1) { perror("listen()"); return 1; }
+
+    pid_t fork_result = fork();
+    switch (fork_result) {
+        case -1: perror("fork()"); return 1;
+        case 0: exec_am_broadcast(argc, argv, input_address_string, output_address_string);
+    }
+
+    struct sockaddr_un remote_addr;
+    socklen_t addrlen = sizeof(remote_addr);
+    int input_client_socket = accept(input_server_socket, (struct sockaddr*) &remote_addr, &addrlen);
+
+    pthread_t transmit_thread;
+    pthread_create(&transmit_thread, NULL, transmit_stdin_to_socket, &output_server_socket);
+
+    transmit_socket_to_stdout(input_client_socket);
+
+    return 0;
+}
+

.gitignore
@@ -1,0 +1,1 @@
+termux-api

Makefile
@@ -1,0 +1,11 @@
+CFLAGS += -std=c11 -Wall -Wextra -pedantic -Werror
+PREFIX ?= /data/data/com.termux/files/usr
+
+termux-api: termux-api.c
+
+install: termux-api
+	mkdir -p $(PREFIX)/bin/ $(PREFIX)/libexec/
+	install termux-api $(PREFIX)/libexec/
+	install scripts/* $(PREFIX)/bin/
+
+.PHONY: install

scripts/termux-battery-status
@@ -1,0 +1,22 @@
+#!/bin/sh
+set -e -u
+
+SCRIPTNAME=termux-battery-status
+show_usage () {
+    echo "Usage: $SCRIPTNAME"
+    echo "Get the status of the device battery."
+    exit 0
+}
+
+while getopts :h option
+do
+    case "$option" in
+        h) show_usage;;
+        ?) echo "$SCRIPTNAME: illegal option -$OPTARG"; exit 1;
+    esac
+done
+shift $(($OPTIND-1))
+
+if [ $# != 0 ]; then echo "$SCRIPTNAME: too many arguments"; exit 1; fi
+
+@TERMUX_API@ BatteryStatus

scripts/termux-camera-info
@@ -1,0 +1,22 @@
+#!/bin/sh
+set -e -u
+
+SCRIPTNAME=termux-camera-info
+show_usage () {
+    echo "Usage: $SCRIPTNAME"
+    echo "Get information about device camera(s)."
+    exit 0
+}
+
+while getopts :h option
+do
+    case "$option" in
+        h) show_usage;;
+        ?) echo "$SCRIPTNAME: illegal option -$OPTARG"; exit 1;
+    esac
+done
+shift $(($OPTIND-1))
+
+if [ $# != 0 ]; then echo "$SCRIPTNAME: too many arguments"; exit 1; fi
+
+@TERMUX_API@ CameraInfo

scripts/termux-camera-photo
@@ -1,0 +1,30 @@
+#!/bin/sh
+set -e -u
+
+SCRIPTNAME=termux-camera-photo
+show_usage () {
+    echo "Usage: termux-camera-photo [-c camera-id] output-file"
+    echo "Take a photo and save it to a file in JPEG format."
+    echo "  -c camera-id  ID of the camera to use (see termux-camera-info), default: 0"
+    exit 0
+}
+
+
+PARAMS=""
+while getopts :hc: option
+do
+    case "$option" in
+        h) show_usage;;
+        c) PARAMS="--es camera $OPTARG";;
+        ?) echo "$SCRIPTNAME: illegal option -$OPTARG"; exit 1;
+    esac
+done
+shift $(($OPTIND-1))
+
+if [ $# = 0 ]; then echo "$SCRIPTNAME: missing file argument"; exit 1; fi
+if [ $# != 1 ]; then echo "$SCRIPTNAME: too many arguments"; exit 1; fi
+
+touch $1
+PARAMS="$PARAMS --es file `realpath $1`"
+
+@TERMUX_API@ CameraPhoto $PARAMS

scripts/termux-clipboard-get
@@ -1,0 +1,22 @@
+#!/bin/sh
+set -e -u
+
+SCRIPTNAME=termux-clipboard-get
+show_usage () {
+    echo "Usage: $SCRIPTNAME"
+    echo "Get the system clipboard text."
+    exit 0
+}
+
+while getopts :h option
+do
+    case "$option" in
+	h) show_usage;;
+	?) echo "$SCRIPTNAME: illegal option -$OPTARG"; exit 1;
+    esac
+done
+shift $(($OPTIND-1))
+
+if [ $# != 0 ]; then echo "$SCRIPTNAME: too many arguments"; exit 1; fi
+
+@TERMUX_API@ Clipboard

scripts/termux-clipboard-set
@@ -1,0 +1,26 @@
+#!/bin/sh
+set -e -u
+
+SCRIPTNAME=termux-clipboard-set
+show_usage () {
+    echo "Usage: $SCRIPTNAME [text]"
+    echo "Set the system clipboard text. The text to set is either supplied as arguments or read from stdin if no arguments are given."
+    exit 0
+}
+
+while getopts :h option
+do
+    case "$option" in
+	h) show_usage;;
+	?) echo "$SCRIPTNAME: illegal option -$OPTARG"; exit 1;
+    esac
+done
+shift $(($OPTIND-1))
+
+CMD="@TERMUX_API@ Clipboard -e api_version 2 --ez set true"
+if [ $# = 0 ]; then
+	$CMD
+else
+	echo $@ | $CMD
+fi
+

commit 0edd4d450710e399c1e3e4f44558eecc0f78a068

Fixed: only malloc as many bytes as are actully needed
And make malloc&memcpy handling clearer.

Before we potentially had an element with uninitialised memory, see
discussion in termux/termux-api-package#203.

Fixes: 1ec1334b54e0 ("Initial push")

termux-api.c
@@ -246,12 +246,19 @@ _Noreturn void exec_am_broadcast(int argc, char** argv,
     // Close stdin:
     close(STDIN_FILENO);
 
-    int const extra_args = 15; // Including ending NULL.
-    char** child_argv = malloc((sizeof(char*)) * (argc + extra_args));
-    if (child_argv == NULL) {
+    const int child_pre_argc = 14;
+    const int child_post_argc = argc - 1; // Except `argv[0]`.
+    const int child_argc = child_pre_argc + child_post_argc;
+
+    size_t child_argv_size = (sizeof(char*)) * (child_argc + 1); // Including trailing `NULL`.
+    // Do not directly cast, otherwise can trigger null pointer dereference if `NULL` is returned.
+    void* result = malloc(child_argv_size);
+    if (result == NULL) {
         perror("malloc failed for am child args");
         exit(1);
     }
+
+    char **child_argv = (char **) result;
 
     child_argv[0] = "am";
     child_argv[1] = "broadcast";
@@ -268,13 +275,12 @@ _Noreturn void exec_am_broadcast(int argc, char** argv,
     child_argv[11] = input_address_string;
     child_argv[12] = "--es";
     child_argv[13] = "api_method";
-    child_argv[14] = argv[1];
 
-    // Copy the remaining arguments -2 for first binary and second api name:
-    memcpy(child_argv + extra_args, argv + 2, (argc-1) * sizeof(char*));
+    // Copy the remaining arguments except `argv[0]`, `argv[1]` should be `api_method` extra value:
+    memcpy(child_argv + child_pre_argc, argv + 1, child_post_argc * sizeof(char*));
 
     // End with NULL:
-    child_argv[argc + extra_args - 1] = NULL;
+    child_argv[child_argc] = NULL;
 
     // Use an a executable taking care of PATH and LD_LIBRARY_PATH:
     execv(PREFIX "/bin/am", child_argv);