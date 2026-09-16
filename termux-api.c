9e7f1531e1aa4a9c1e261a2dbde4de93653c366e
agnostic-apollo <agnosticapollo@gmail.com>
2026-09-17 03:12:40 +0500

    Release: 0.60.0

--- termux-api.c
+++ termux-api.c
@@ -22,7 +22,7 @@
 
 #include "termux-api.h"
 
-#define TERMUX_API_PACKAGE_VERSION "0.59.1"
+#define TERMUX_API_PACKAGE_VERSION "0.60.0"
 
 #ifndef PREFIX
 # define PREFIX "/data/data/com.termux/files/usr"
